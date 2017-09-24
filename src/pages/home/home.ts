c2f6e3fc9db70d5e7f4fd82a1f83ea2350e102d4
goltra <goltratec@gmail.com>
2017-09-24 11:09:25 +0200

    primer commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, LoadingController } from 'ionic-angular';
+import Tesseract from 'tesseract.js';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage {
+  @ViewChild('imagen')
+  private imagen: ElementRef;
+
+  constructor(public navCtrl: NavController, public loading: LoadingController) {
 
   }
-
+  analiza() {
+    let load = this.loading.create({ content: 'Analizando la imagen' });
+    load.present();
+    Tesseract.recognize(this.imagen.nativeElement.src,{lang:'spa'})
+    .then(data => {
+      load.dismiss();
+      console.log('analizando');
+      console.log(data)
+      alert(data.text);
+      // console.log(text);
+    }).catch(err => {
+      load.dismiss();
+      console.log('error analizando');
+    });
+  }
 }
